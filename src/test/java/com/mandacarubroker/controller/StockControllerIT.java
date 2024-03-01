8f9c84d78ccb656ac579061f9484444adbe3317d
peaga <pauloherbert@alu.ufc.br>
2024-03-01 00:58:26 -0300

    add: test database config with test-properties file

--- src/test/java/com/mandacarubroker/controller/StockControllerIT.java
+++ src/test/java/com/mandacarubroker/controller/StockControllerIT.java
@@ -12,10 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
-
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -23,6 +23,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(classes = MandacaruBrokerApplication.class)
+@ActiveProfiles(value = "test")
 @AutoConfigureMockMvc
 @Transactional
 class StockControllerIT {
